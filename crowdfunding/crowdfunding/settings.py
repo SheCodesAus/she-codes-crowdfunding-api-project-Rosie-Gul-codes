5c9593e81760956ba8217c1285bcbd71726ef097
Rosie-Gul-codes <guldidar.kizil@gmail.com>
2022-07-05 18:57:29 +1000

    Update settings.py

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-     DJANGO_SECRET_KEY',
+SECRET_KEY = os.environ.get
+     (DJANGO_SECRET_KEY',
      'django-insecure-6f5fiv53l$d=%d_0_8&znvd!6&d3rfy-qowzswx^u)i-p_dsm6'
 )  
 
